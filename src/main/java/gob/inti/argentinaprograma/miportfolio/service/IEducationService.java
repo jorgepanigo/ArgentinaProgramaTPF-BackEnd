48f134abd60d9a4659dc32394a9f539d3c09afef
jorgepanigo <jorgepanigo@gmail.com>
2022-05-15 15:53:51 -0300

    Creando interface para servicio de Education

---
+++ src/main/java/gob/inti/argentinaprograma/miportfolio/service/IEducationService.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gob.inti.argentinaprograma.miportfolio.service;
+
+import gob.inti.argentinaprograma.miportfolio.model.Education;
+import java.util.List;
+
+/**
+ *
+ * @author Jorge Panigo <jorgepanigo@gmail.com>
+ */
+public interface IEducationService {
+    
+    
+    //Devuelve lista de items de aducacion
+    public Education getItem(Long id);
+    
+    //Devuelve un item en paricular
+    public List<Education> getList();
+    
+    //Actualiza datos de un item
+    public void update (Education e);
+    
+    //Elimina un item por por id
+    public void delete (Long id);
+    
+    
+    
+    
+    
+    
+    
+}
